88fea436735fa7413e101d50f60e783286aaf9a8
Pratik Kabade <76637730+pratikkabade@users.noreply.github.com>
2022-07-12 10:54:12 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Header from "./components/Header.svelte";
+  import Counter from "./lib/Counter.svelte";
+  import GetStarted from "./pages/GetStarted.svelte";
+</script>
+
+<main>
+  <Header />
+  <GetStarted />
+  <div class="mainSection noSelect" style="margin-top: 85px;">
+    <div class="subSection">
+      <h3 class="heading noSelect">Sample Function</h3>
+      <Counter />
+    </div>
+  </div>
+</main>
---
+++ src/pages/GetStarted.svelte
@@ -0,0 +1,20 @@
+<div>
+  <h1 class="heading noSelect" style="margin-top: 85px;">Get Started</h1>
+  <div class="mainSection noSelect">
+    <div class="subSection">
+      <h1>Install</h1>
+      <p style="user-select: all;">npm init vite my-app -- --template svelte</p>
+    </div>
+    <div class="subSection">
+      <h1>Start</h1>
+      <p style="user-select: all;">npm install</p>
+      <p style="user-select: all;">npm run dev</p>
+    </div>
+  </div>
+</div>
+
+<style>
+  .heading {
+    text-align: center;
+  }
+</style>
